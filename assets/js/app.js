a63cb6757b9e31a614047618a6d6ad77ce8bf1a3
jojohve <joseph.laluna@outlook.it>
2024-04-17 17:53:56 +0200

    parte due

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,4 @@
-async function loadFile() {
-    try {
-        let news = await fetchApi("https://hacker-news.firebaseio.com/v0/newstories.json");
-        let first10 = news.slice(0,10);
-
-        for (let id of first10) {
-            let detailOneNews = await detailFetchApi(id);
-            displayDetail (detailOneNews);
-        }
-    } catch (error) {
-        console.log("Si è verificato un errore", error);
-    }
-}
-
-async function fetchApi() {
+async function fetchApi(url) {
     let response = await fetch(url);
 
     if (!response.ok) {
@@ -21,34 +7,55 @@ async function fetchApi() {
     return await response.json();
 }
 
-async function detailFetchApi() {
-    let apiUrl = "https://hacker-news.firebaseio.com/v0/item/${id}.json";
+async function detailFetchApi(id) {
+    let apiUrl = `https://hacker-news.firebaseio.com/v0/item/${id}.json`;
     let detailOneNews = await fetchApi(apiUrl);
     return detailOneNews;
 }
 
-async function loadMoreFile() {
-    let next10 = news.slice(lastNews, lastNews + 10);
+async function loadFile() {
+    try {
+        let news = await fetchApi("https://hacker-news.firebaseio.com/v0/newstories.json");
+        let first10 = news.slice(0, 10);
 
-    for (let id of next10) {
-        let detailOneNews = await detailFetchApi(id);
-        displayDetail (detailOneNews);
+        for (let id of first10) {
+            let detailOneNews = await detailFetchApi(id);
+            displayNews(detailOneNews);
+        }
+
+        let loadMore = document.getElementById("loadMoreButton");
+        loadMore.addEventListener("click", loadMoreFile);
+
+        let lastNews = 10;
+
+        async function loadMoreFile() {
+            let next10 = news.slice(lastNews, lastNews + 10);
+
+            for (let id of next10) {
+                let detailOneNews = await detailFetchApi(id);
+                displayNews(detailOneNews);
+            }
+
+            lastNews += 10;
+        }
+    } catch (error) {
+        console.log("Si è verificato un errore", error);
     }
 }
 
-function displayNews (oneNews) {
+function displayNews(news) {
     let newsDetailDiv = document.createElement("div");
     newsDetailDiv.classList.add("news-detail");
 
     let title = document.createElement("h2")
-    title.textContent = oneNews.title;
+    title.textContent = news.title;
 
     let link = document.createElement("a");
-    link.href = oneNews.url;
+    link.href = news.url;
     link.textContent = "Link alla notizia";
 
     let date = document.createElement("label");
-    let newsDate = new Date (oneNews.time * 1000);
+    let newsDate = new Date(news.time * 1000);
     date.textContent = "Data: " + newsDate.toLocaleDateString();
 
     newsDetailDiv.appendChild(title);
